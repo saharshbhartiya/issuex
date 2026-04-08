e296ac232f73b74216eea22cc318efaeb3580109
saharshbhartiya <saharshbhartiya@gmail.com>
2026-04-08 16:54:08 +0530

    Enabled : CORS for frontend access

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app.api.routes_issue import router as issue_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 
 @app.get("/")
 def home():
     return {"message" : "IssueX running"}
 
 app.include_router(issue_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
